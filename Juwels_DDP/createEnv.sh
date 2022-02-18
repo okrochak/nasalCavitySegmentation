aab38ca66906619878025413027f7b32179d6e81
Eray Inanc <e.inanc@fz-juelich.de>
2022-02-18 10:14:25 +0100

    Update createEnv.sh according to stage 2022

--- Juwels_DDP/createEnv.sh
+++ Juwels_DDP/createEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # author: EI
-# version: 210601a
+# version: 220211a
 # creates machine specific python env
 
 # set modules
@@ -9,55 +9,142 @@ module --force purge
 
 # get sys info
 cDir=$PWD
-sysN=$(eval "uname -n | cut -f2- -d.")
+sysN="$(uname -n | cut -f2- -d.)"
 echo "system:${sysN}"
 echo
 
 cont1=false
 if [ "$sysN" = 'deepv' ] ; then
-   module use $OTHERSTAGES
-   ml Stages/2020 GCCcore/.9.3.0 GCC/9.3.0 ParaStationMPI/5.4.7-1-mt Python
-   cont1=true
+  module use $OTHERSTAGES
+  ml GCC ParaStationMPI/5.4.9-1-mt Python cuDNN NCCL Python
+  cont1=true
 elif [ "$sysN" = 'juwels' ] ; then
-   ml GCC ParaStationMPI Python
-   cont1=true
+  ml Stages/2022 GCC ParaStationMPI Python CMake
+  cont1=true
+elif [ "$sysN" = 'jureca' ] ; then
+  #ml Stages/2022 GCC ParaStationMPI Python CMake NCCL libaio # Horovod issues with pscom??
+  ml Stages/2022 GCC OpenMPI Python NCCL cuDNN libaio CMake
+  cont1=true
 else
-   echo
-   echo 'unknown system detected'
-   echo 'canceling'
-   echo
+  echo
+  echo 'unknown system detected'
+  echo 'canceling'
+  echo
 fi
 echo "modules loaded"
 echo
 
+# get python version
+pver="$(python --version 2>&1 | awk {'print $2'} | cut -f1-2 -d.)"
+echo "python version is ${pver}"
+echo
+
 if [ "$cont1" = true ] ; then
-   pVer=$(eval "python --version")
-   pDir=$(eval "which python")
+  if [ -d "${cDir}/envAI_${sysN}" ];then
+    echo 'env already exist'
+    echo
 
-   echo "python ver:${pVer}"
-   echo "python dir:${pDir}"
-   echo
+    source envAI_${sysN}/bin/activate
+  else
+    # create env
+    python3 -m venv envAI_${sysN}
 
-   # create env
-   python -m venv envAI_${sysN}
+    # get headers for pip
+    if [ -f "${cDir}/envAI_${sysN}/bin/pip3" ]; then
+      echo 'pip already exist'
+    else
+      cp "$(which pip3)" $cDir/envAI_${sysN}/bin/
+      ln -s $cDir/envAI_${sysN}/bin/pip3 $cDir/envAI_${sysN}/bin/pip${pver}
+      var="#!$cDir/envAI_${sysN}/bin/python${pver}"
+      sed -i "1s|.*|$var|" $cDir/envAI_${sysN}/bin/pip3
+    fi
 
-   # activate env
-   source envAI_${sysN}/bin/activate
+    # activate env
+    source envAI_${sysN}/bin/activate
 
-   pVer=$(eval "python --version")
-   pDir=$(eval "which python")
-   echo "python env ver:${pVer}"
-   echo "python env dir:${pDir}"
-   echo
+    echo "a new env is created in ${cDir}"
+    echo "activation is done via:"
+    echo "source ${cDir}/envAI_${sysN}/bin/activate"
+  fi
+fi
 
-   # install stuff
-   TMPDIR=${cDir} pip install \
-      torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 \
-      -f https://download.pytorch.org/whl/torch_stable.html --no-cache-dir
+# install torch
+if [ -f "${cDir}/envAI_${sysN}/bin/torchrun" ]; then
+  echo 'Torch already installed'
+  echo
+else
+  export TMPDIR=${cDir}
 
-   echo "a new env is created in ${cDir}"
-   echo "activation is done via:"
-   echo "source ${cDir}/envAI_${sysN}/bin/activate"
+  pip3 install \
+     torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio==0.10.2+cu113 \
+     -f https://download.pytorch.org/whl/cu113/torch_stable.html --no-cache-dir
+fi
+
+# install horovod
+if [ -f "${cDir}/envAI_${sysN}/bin/horovodrun" ]; then
+  echo 'Horovod already installed'
+  echo
+else
+  export HOROVOD_GPU=CUDA
+  export HOROVOD_GPU_OPERATIONS=NCCL
+  export HOROVOD_WITH_PYTORCH=1
+  export TMPDIR=${cDir}
+
+  pip3 install --no-cache-dir horovod --ignore-installed
+fi
+
+# install deepspeed
+if [ -f "${cDir}/envAI_${sysN}/bin/deepspeed" ]; then
+  echo 'DeepSpeed already installed'
+  echo
+else
+  export DS_BUILD_OPS=1
+  # if above not working?? recursion error use this
+  #export DS_BUILD_FUSED_ADAM=1
+  #export DS_BUILD_UTILS=1
+  export TMPDIR=${cDir}
+
+  pip3 install --no-cache-dir DeepSpeed
+
+  add this to .../deepspeed/launcher/launch.py l.70
+  var='    argsy1.node_rank=int(os.environ.get("SLURM_PROCID",0))'
+  sed -i "85s|.*|$var|" $cDir/envAI_${sysN}/lib/python${pver}/site-packages/deepspeed/launcher/launch.py
+fi
+
+# install heat
+if [ -d "${cDir}/envAI_${sysN}/lib/python${pver}/site-packages/heat" ]; then
+  echo 'HeAT already installed'
+  echo
+else
+  export TMPDIR=${cDir}
+
+  # need to modify setup.py to accep torch>1.9 for heat
+  wget https://files.pythonhosted.org/packages/5d/3a/4781f1e6910753bfdfa6712c83c732c60e675d8de14983926a0d9306c7a6/heat-1.1.1.tar.gz
+  tar xzf heat-1.1.1.tar.gz
+  var='        "torch>=1.7.0",'
+  sed -i "36s|.*|$var|" heat-1.1.1/setup.py
+  var='        "torchvision>=0.8.0",'
+  sed -i "39s|.*|$var|" heat-1.1.1/setup.py
+
+  # create tar again!
+  rm -rf heat-1.1.1.tar.gz
+  tar czf heat-1.1.1.tar.gz heat-1.1.1
+  rm -rf heat-1.1.1
+
+  pip3 install --no-cache-dir 'heat-1.1.1.tar.gz[hdf5,netcdf]'
+
+  rm -rf heat-1.1.1.tar.gz
+fi
+
+# get rest of the libraries$
+if [ "$cont1" = true ] ; then
+  # install rest
+  pip3 install -r reqs.txt --ignore-installed
+
+  # modify l.4 of /torchnlp/_third_party/weighted_random_sampler.py
+  var='int_classes = int'
+  sed -i "4s|.*|$var|" \
+    $cDir/envAI_${sysN}/lib/python${pver}/site-packages/torchnlp/_third_party/weighted_random_sampler.py
 fi
 
 #eof
