0feb7c1b46e7ae949cdb38dfed1aadba1b2c83db
Eray Inanc <e.inanc@fz-juelich.de>
2022-04-08 15:16:40 +0200

    latest bug fixes

--- Jureca_DeepSpeed/DS_startscript_deep.sh
+++ Jureca_DeepSpeed/DS_startscript_deep.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # general configuration of the job
-#SBATCH --job-name=DSTest
+#SBATCH --job-name=DStest
 #SBATCH --account=raise-ctp1
 #SBATCH --mail-user=
 #SBATCH --mail-type=ALL
 #SBATCH --output=job.out
 #SBATCH --error=job.err
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 
 # configure node and process count on the CM
 #SBATCH --partition=dc-gpu
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --exclusive
 
 # gres options have to be disabled for deepv
@@ -22,18 +22,20 @@
 
 # parameters
 debug=false # do nccl debug
-epochs=10   # epochs
+epochs=1    # epochs
+be='nccl'   # backend
 lr=0.01     # learning rate
-bs=96       # batch-size
+bs=3        # batch-size
 
 # AT
-dataDir="/p/scratch/raise-ctp1/T31_LD/"
+dataDir='/p/scratch/raise-ctp1/inanc2/T31_LD/'
 COMMAND="DS_pytorch_AT.py"
-EXEC="$COMMAND \
-  --batch-size $bs \
-  --epochs $epochs \
-  --lr $lr \
-  --nworker $SLURM_CPUS_PER_TASK \
+
+EXEC=$COMMAND" --batch-size $bs
+  --epochs $epochs
+  --backend $be
+  --nworker $SLURM_CPUS_PER_TASK
+  --benchrun
   --data-dir $dataDir"
 
 # set modules
@@ -45,7 +47,7 @@ source /p/project/raise-ctp1/RAISE/envAI_jureca/bin/activate
 # sleep a sec
 sleep 1
 
-# job info 
+# job info
 echo "DEBUG: EXECUTE: $EXEC"
 echo "DEBUG: SLURM_JOB_ID: $SLURM_JOB_ID"
 echo "DEBUG: SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
@@ -55,7 +57,7 @@ echo "DEBUG: SLURM_TASKS_PER_NODE: $SLURM_TASKS_PER_NODE"
 echo "DEBUG: SLURM_SUBMIT_HOST: $SLURM_SUBMIT_HOST"
 echo "DEBUG: SLURMD_NODENAME: $SLURMD_NODENAME"
 echo "DEBUG: SLURM_NODEID: $SLURM_NODEID"
-echo "DEBUG: SLURM_LOCALID: $SLURM_LOCALID" 
+echo "DEBUG: SLURM_LOCALID: $SLURM_LOCALID"
 echo "DEBUG: SLURM_PROCID: $SLURM_PROCID"
 echo "DEBUG: CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 if [ "$debug" = true ] ; then
@@ -65,13 +67,14 @@ echo
 
 # set comm
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export OMP_NUM_THREADS=1
 if [ "$SLURM_CPUS_PER_TASK" > 0 ] ; then
   export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 fi
 
 #### do not change this part
 # create node-list
-sysN=$(scontrol show hostnames)
+sysN=$(eval "scontrol show hostnames")
 for i in $sysN; do
   x+=\"$i\":[$CUDA_VISIBLE_DEVICES],
 done
@@ -81,7 +84,8 @@ WID=`echo {${x::-1}} | base64 -w 0`
 sed -i "2s|.*|  \"train_micro_batch_size_per_gpu\": ${bs},|" DS_config.json
 ####
 
-srun python3 -m deepspeed.launcher.launch \
+# launch
+srun python -m deepspeed.launcher.launch \
   --node_rank $SLURM_PROCID \
   --master_addr ${SLURMD_NODENAME}i \
   --master_port 29500 \
