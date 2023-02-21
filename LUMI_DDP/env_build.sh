27396b5134afa397970c2d22cd2dc758c5774e7f
Eray Inanc <e.inanc@fz-juelich.de>
2023-02-21 15:06:36 +0100

    Update env_build.sh

--- LUMI_DDP/env_build.sh
+++ LUMI_DDP/env_build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # author: EI
-# version: 221208a
+# version: 230214a
 # creates python env with torch on LUMI
 
 # parameters
 nameE='envAI_LUMI'
 
 # set modules
-ml LUMI/22.08 partition/EAP rocm/5.0.2 ModulePowerUser/LUMI buildtools cray-python/3.9.12.1 craype-accel-amd-gfx90a
+#ml LUMI/22.08 partition/G rocm/5.0.2 ModulePowerUser/LUMI buildtools cray-python/3.9.12.1
 
-# set env vars special for LUMI
+# module package suggested by LUMI support
+ml use /pfs/lustrep2/projappl/project_462000125/samantao-public/mymodules
+ml aws-ofi-rccl/sam-rocm-5.3.3.lua
+
+# set env vars special for LUMI with rocm 5.3.0
 export LD_LIBRARY_PATH=$HIP_LIB_PATH:$LD_LIBRARY_PATH
 export MPICH_GPU_SUPPORT_ENABLED=1
 
@@ -38,8 +42,11 @@ PYTHONPATH=$PWD/$nameE/lib/python3.9:/opt/cray/pe/python/3.9.12.1
 if [ -f "$PWD/$nameE/bin/torchrun" ]; then
   echo 'torch already installed'
 else
-  pip${pver} install torch==1.12.1+rocm5.0 torchvision==0.13.1+rocm5.0 torchaudio==0.12.1+rocm5.0 \
-          --extra-index-url https://download.pytorch.org/whl/rocm5.0/ --no-cache-dir
+  #pip${pver} install torch==1.12.1+rocm5.0 torchvision==0.13.1+rocm5.0 torchaudio==0.12.1+rocm5.0 \
+  #        --extra-index-url https://download.pytorch.org/whl/rocm5.0/ --no-cache-dir
+  
+  pip${pver} install --pre torch torchvision torchaudio \
+          --extra-index-url https://download.pytorch.org/whl/nightly/rocm5.3 --no-cache-dir
 fi 
 
 # get rest of the libs
