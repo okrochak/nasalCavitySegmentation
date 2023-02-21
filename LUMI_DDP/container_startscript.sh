7142fcba7dc72041fd0ffa10d3aa04f706e1e462
Eray Inanc <e.inanc@fz-juelich.de>
2023-02-21 15:02:57 +0100

    Update container_startscript.sh

--- LUMI_DDP/container_startscript.sh
+++ LUMI_DDP/container_startscript.sh
@@ -7,13 +7,13 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=job.out
 #SBATCH --error=job.err
-#SBATCH --time=0-00:15:00
+#SBATCH --time=0-00:30:00
 
 # configure node and process count
-# SBATCH --partition=dev-g
-#SBATCH --partition=small-g
+#SBATCH --partition=dev-g
+# SBATCH --partition=small-g
 # SBATCH --partition=standard-g
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=7
 #SBATCH --gpus-per-node=8
@@ -21,51 +21,45 @@
 
 # parameters
 
-## MNIST
-#dataDir='/scratch/project_465000280/RAISE_datasets/data_MNIST/'
-#COMMAND="DDP_pytorch_mnist.py"
-#EXEC="$COMMAND --backend nccl --epochs 10 --concM 10 --nworker $SLURM_CPUS_PER_TASK\
-# --data-dir $dataDir"
-
 # AT
-dataDir='/scratch/project_465000280/RAISE_datasets/actuated_tbl/'
+dataDir='/scratch/${SBATCH_ACCOUNT}/RAISE_datasets/actuated_tbl/'
 COMMAND="DDP_pytorch_AT.py"
 EXEC="$COMMAND --epochs 1 --batch-size 1 --concM 1 --benchrun \
        --nworker $SLURM_CPUS_PER_TASK --data-dir $dataDir"
 
+
 ### do not modify below ###
 
-# set modules
-ml LUMI/22.08 partition/G rocm/5.0.2 ModulePowerUser/LUMI buildtools cray-python/3.9.12.1
 
-# set env vars 
+# set modules
+ml LUMI/22.08 partition/G singularity-bindings aws-ofi-rccl
+
+# set env vars
 unset GREP_OPTIONS
+#-hip
 export HIP_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 export ROCR_VISIBLE_DEVICES=$SLURM_LOCALID
 export LD_LIBRARY_PATH=$HIP_LIB_PATH:$LD_LIBRARY_PATH
-export MPICH_GPU_SUPPORT_ENABLED=1
+#-threads
 export OMP_NUM_THREADS=1
 if [ "$SLURM_CPUS_PER_TASK" > 0 ] ; then
   export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 fi
-
-# miopen
-# check: https://rocmsoftwareplatform.github.io/MIOpen/doc/html/install.html
-export MIOPEN_USER_DB_PATH="/tmp/sam-miopen-cache-$SLURM_PROCID"
-export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
-#rm -rf $MIOPEN_USER_DB_PATH
-mkdir -p $MIOPEN_USER_DB_PATH
-#export MIOPEN_DEBUG_DISABLE_FIND_DB=1
-#export MIOPEN_DISABLE_CACHE=1
-export MIOPEN_FIND_ENFORCE=5
-
-# nccl
-# check: https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/env.html
-export NCCL_DEBUG=WARN
+#-nccl
 export NCCL_SOCKET_IFNAME=hsn
-#export NCCL_SOCKET_IFNAM=hsn0,hsn1,hsn2,hsn3
+export NCCL_NET_GDR_LEVEL=3
+#-miopen
+export MIOPEN_USER_DB_PATH=/tmp/${USER}-miopen-cache-${SLURM_JOB_ID}
+export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
+#-rocm
+export CXI_FORK_SAFE=1
+export CXI_FORK_SAFE_HP=1
+export FI_CXI_DISABLE_CQ_HUGETLB=1
+#-container
+export SINGULARITYENV_LD_LIBRARY_PATH=/opt/ompi/lib:${EBROOTAWSMINOFIMINRCCL}/lib:/opt/cray/xpmem/2.4.4-2.3_9.1__gff0e1d9.shasta/lib64:${SINGULARITYENV_LD_LIBRARY_PATH}
+export SCRATCH="/scratch/${SBATCH_ACCOUNT}"
 
-# job info 
+# job info
 echo "DEBUG: TIME: $(date)"
 echo "DEBUG: EXECUTE: $EXEC"
 echo "DEBUG: SLURM_SUBMIT_DIR: $SLURM_SUBMIT_DIR"
@@ -78,21 +72,14 @@ echo "DEBUG: SLURM_SUBMIT_HOST: $SLURM_SUBMIT_HOST"
 echo "DEBUG: SLURMD_NODENAME: $SLURMD_NODENAME"
 echo "DEBUG: HIP_VISIBLE_DEVICES: $HIP_VISIBLE_DEVICES"
 
-# required if --cpu-bind is required, bind with --cpu-bind=mask_cpu:$MASKS
-MASKS="ff000000000000,ff00000000000000,ff0000,ff000000,ff,ff00,ff00000000,ff0000000000"
-
-# singularity does not bind SCRATCH at this moment!
-export SCRATCH_P="/scratch/project_465000280"
-
-# launch
-srun --cpu-bind=none bash -c \
-    "singularity exec --rocm -B $SCRATCH_P torch_rocm_docker.sif torchrun \
+srun --cpu-bind=none singularity exec --rocm -B /appl:/appl -B $SCRATCH:$SCRATCH \
+    torch_rocm.sif bash -c ". torch_rocm_env/bin/activate; torchrun \
     --log_dir='logs' \
     --nnodes=$SLURM_NNODES \
     --nproc_per_node=$SLURM_GPUS_PER_NODE \
     --rdzv_id=$SLURM_JOB_ID \
     --rdzv_backend=c10d \
-    --rdzv_endpoint='$(scontrol show hostname "$SLURM_NODELIST" | head -n 1)':34567 \
+    --rdzv_endpoint='$(scontrol show hostname "$SLURM_NODELIST" | head -n 1)':39591 \
     $EXEC"
 
 # eof
