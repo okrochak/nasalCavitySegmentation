ec191137d59430b21f5caa3dd3fc923af96f7dea
Eray Inanc <e.inanc@fz-juelich.de>
2023-03-08 11:36:58 +0100

    Update benchScript.sh

--- BENCHMARKS/benchScript.sh
+++ BENCHMARKS/benchScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # general configuration of the job
-#SBATCH --job-name=PyTorchJUBE
+#SBATCH --job-name=TorchJUBE
 #SBATCH --account=#ACC#
 #SBATCH --mail-user=
 #SBATCH --mail-type=ALL
@@ -42,9 +42,11 @@ export OMP_NUM_THREADS=1
 if [ "$SLURM_CPUS_PER_TASK" > 0 ] ; then
   export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 fi
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
 
 # debug 
 sleep 1
+echo "DEBUG: TIME: $(date)"
 echo "DEBUG: EXECUTE: $EXEC"
 echo "DEBUG: SLURM_JOB_ID: $SLURM_JOB_ID"
 echo "DEBUG: SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
@@ -52,12 +54,13 @@ echo "DEBUG: SLURM_NNODES: $SLURM_NNODES"
 echo "DEBUG: SLURM_NTASKS: $SLURM_NTASKS"
 echo "DEBUG: SLURM_TASKS_PER_NODE: $SLURM_TASKS_PER_NODE"
 echo "DEBUG: SLURM_SUBMIT_HOST: $SLURM_SUBMIT_HOST"
+echo "DEBUG: SLURMD_NODENAME: $SLURMD_NODENAME"
 echo "DEBUG: SLURM_NODEID: $SLURM_NODEID"
-echo "DEBUG: SLURM_LOCALID: $SLURM_LOCALID"
-echo "DEBUG: SLURM_PROCID: $SLURM_PROCID"
+echo "DEBUG: SLURM_GPUS_PER_NODE: $SLURM_GPUS_PER_NODE"
+echo "DEBUG: CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
 # launch
-srun bash -c "torchrun \
+srun --cpu-bind=none bash -c "torchrun \
     --log_dir='logs' \
     --nnodes=$SLURM_NNODES \
     --nproc_per_node=$SLURM_GPUS_PER_NODE \
