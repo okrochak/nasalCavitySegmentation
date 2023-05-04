0927f87b8791be2b49a8c71a100b53a0fbbab7b5
Marcel Aach <m.aach@fz-juelich.de>
2023-05-04 10:54:18 +0200

    Update 3 files
    - /Jureca_RayTune/Ray_2.4/ASHA/cifar_tun_asha.py
    - /Jureca_RayTune/Ray_2.4/ASHA/cifar_tune_asha.py
    - /Jureca_RayTune/Ray_2.4/ASHA/jureca_ray_startscript.sh

--- Jureca_RayTune/Ray_2.4/ASHA/jureca_ray_startscript.sh
+++ Jureca_RayTune/Ray_2.4/ASHA/jureca_ray_startscript.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2206
 
 #SBATCH --job-name=ray_cifar_test
-#SBATCH --account=raise-ctp2
+#SBATCH --account=
 #SBATCH --output=ray_test_cifar.out
 #SBATCH --error=ray_test_cifar.err
 #SBATCH --partition=dc-gpu
@@ -18,9 +18,9 @@ ml --force purge
 
 ml Stages/2023  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7 torchvision/0.13.1-CUDA-11.7
 
-source /p/project/cslfse/aach1/NAS/ray_2/ray_tune_env/bin/activate
+source ray_tune_env/bin/activate
 
-COMMAND="cifar_tune.py --scheduler ASHA --num-samples 12 --par-workers 2 --max-iterations 2 --data-dir /p/scratch/raise-ctp2/cifar10/data "
+COMMAND="cifar_tune_asha.py --scheduler ASHA --num-samples 12 --par-workers 2 --max-iterations 2 --data-dir /p/scratch/raise-ctp2/cifar10/data "
 
 echo $COMMAND
 
